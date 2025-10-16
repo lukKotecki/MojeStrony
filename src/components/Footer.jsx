a64110f27d86b069eb88fde4171803ab90d72312
Lukas <lukkotecki@gmail.com>
2025-10-16 23:20:48 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function  Footer(){
 
     return (
         <footer>
-            <p>MojeStrony.pl copyright</p>
+            <p>MojeStrony.pl copyright 2025</p>
         </footer>
     )
 }
